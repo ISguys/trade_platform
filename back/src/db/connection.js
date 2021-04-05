112ccf3a02737e3218f4de6ac1c59b5e0c9d04de
JohnyKovalenko1337 <13371488zeus@gmail.com>
2021-04-05 12:41:47 +0300

    [Config] added basic configuration for main file and db

---
+++ back/src/db/connection.js
@@ -0,0 +1,10 @@
+const { Pool } = require('pg');
+const pool = new Pool({
+    host: 'localhost',
+    user: 'database-user',
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
+});
+
+module.exports = pool;
