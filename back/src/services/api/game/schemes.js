7bfbf29a420a17646158e8b3a58d7b3e62e60e28
alnkravchenko <alenakravchenko1@gmail.com>
2021-04-07 16:50:41 +0300

    [Games] add routes, controllers and schemes

---
+++ back/src/services/api/game/schemes.js
@@ -0,0 +1,115 @@
+const schemes = {};
+// get
+schemes.getAll = {
+    response: {
+        200: {
+            type: ['array', 'null'],
+            items: {
+                type: 'object',
+                properties: {
+                    gameId: { type: 'integer' },
+                    streamPrice: { type: 'number' },
+                    steamLink: { type: 'string' },
+                    imageLink: { type: 'string' },
+                    description: { type: 'string' },
+                },
+            },
+        },
+    },
+};
+// get
+schemes.getGameById = {
+    querystring: {
+        type: 'object',
+        required: ['gameId'],
+        properties: {
+            gameId: { type: 'integer' },
+        },
+    },
+
+    response: {
+        200: {
+            type: ['array', 'null'],
+            items: {
+                type: 'object',
+                properties: {
+                    gameId: { type: 'integer' },
+                    streamPrice: { type: 'number' },
+                    steamLink: { type: 'string' },
+                    imageLink: { type: 'string' },
+                    description: { type: 'string' },
+                },
+            },
+        },
+    },
+};
+// post
+schemes.addGame = {
+    body: {
+        type: 'object',
+        required: [
+            'steamPrice',
+            'title',
+            'steamLink',
+            'imageLink',
+            'description',
+        ],
+        properties: {
+            steamPrice: { type: 'number' },
+            title: { type: 'string' },
+            steamLink: { type: 'string' },
+            imageLink: { type: 'string' },
+            description: { type: 'string' },
+        },
+    },
+
+    response: {
+        200: {
+            type: 'object',
+            properties: {
+                status: { type: 'string' },
+            },
+        },
+    },
+};
+// put
+schemes.updateGame = {
+    body: {
+        type: 'object',
+        required: ['gameId', 'fields'],
+        properties: {
+            gameId: { type: 'integer' },
+            fields: { type: 'array' },
+        },
+    },
+
+    response: {
+        200: {
+            type: 'object',
+            properties: {
+                status: { type: 'string' },
+            },
+        },
+    },
+};
+//delete
+schemes.deleteGame = {
+    body: {
+        type: 'object',
+        required: ['gameId'],
+        properties: {
+            gameId: { type: 'integer' },
+        },
+    },
+
+    response: {
+        200: {
+            type: 'object',
+            properties: {
+                status: { type: 'string' },
+            },
+        },
+    },
+};
+
+module.exports = schemes;
