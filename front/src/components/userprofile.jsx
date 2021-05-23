7cb85c82b36735b048088341a716bff252e6858e
JohnyKovalenko1337 <13371488zeus@gmail.com>
2021-05-23 16:45:54 +0300

    [Games] fixed front for getting 1 game

--- front/src/components/userprofile.jsx
+++ front/src/components/userprofile.jsx
@@ -2,23 +2,22 @@ import React from 'react';
 import Footer from '../components/Footer';
 
 
-
 const UserProfile = () => (
     <>
         <div id="gamepage">
 
             <div id="frame2">
-                <div id="avatar"> </div>
+                <div id="avatar"></div>
             </div>
 
             <div id="profilename">★†SNIPĖR CÆT†★</div>
-            <div id="Alloffers1"> Ссылка на обмен: </div>
-            <div id="lookinsteam1"> <input size="40"/></div>
+            <div id="Alloffers1"> Ссылка на обмен:</div>
+            <div id="lookinsteam1"><input size="40"/></div>
             <div id="genres1"> Ваши игры на продажу:</div>
-            <div id="description1">  </div>
+            <div id="description1"></div>
 
         </div>
-        <Footer />
+        <Footer/>
     </>
 );
 
