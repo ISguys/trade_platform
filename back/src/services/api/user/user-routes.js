8bac1d6464b74789677675753d55a757da04ede4
alnkravchenko <alenakravchenko1@gmail.com>
2021-06-02 22:56:57 +0300

    [Users] Delete scheme

--- back/src/services/api/user/user-routes.js
+++ back/src/services/api/user/user-routes.js
@@ -1,8 +1,4 @@
 const {
-    getAllSchema, getByIdSchema, getAccountSchema
-} = require('./schema/user-schema');
-
-const {
     getAll, getById, getAccount, update
 } = require('./user-controller');
 
@@ -10,21 +6,18 @@ module.exports = async function(fastify) {
     fastify.route({
         method: 'GET',
         url: '/users/',
-        schema: getAllSchema,
         preValidation: [fastify.tokenValidation],
         handler: getAll,
     });
     fastify.route({
         method: 'GET',
         url: '/users/:userId',
-        schema: getByIdSchema,
         preValidation: [fastify.tokenValidation],
         handler: getById,
     });
     fastify.route({
         method: 'GET',
         url: '/myaccount/:userId',
-/*        schema: getAccountSchema,*/
         preValidation: [fastify.tokenValidation],
         handler: getAccount,
     });
